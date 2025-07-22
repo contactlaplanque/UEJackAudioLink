5e80a981de43d4112abffbd3e7de6b277242735d
Nicolas Désilles <nicolas@ndlabs.fr>
2025-07-22 19:30:04 +0200

    Still testing

--- TeamCity/BuildSteps/packaging.ps1
+++ TeamCity/BuildSteps/packaging.ps1
@@ -5,8 +5,7 @@
       -TargetPlatforms=Win64 `
       -Configuration=Development `
       -VS2022 `
-      -NoCleanStage `
-      -Rocket                              # strip editor-only symbols
+      -NoCleanStage
 
 if ($LASTEXITCODE -ne 0) { throw "BuildPlugin failed with code $LASTEXITCODE" }
 
